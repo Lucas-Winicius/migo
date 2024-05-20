e1972422eaa0d420ab9bcc1e3ba392b2f2e504c8
Lucas-Winicius <lucasclucas7@gmail.com>
2024-05-19 22:18:08 -0300

    responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,43 @@
-import logo from '@/../public/logo.png'
-import Image from 'next/image';
-import Link from 'next/link';
+import logo from "@/../public/logo.png";
+import Image from "next/image";
+import Link from "next/link";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { AlignJustify } from "lucide-react";
+import Links from "./Links";
 
 export default function Header() {
   return (
     <header className="flex items-center justify-between bg-green-500 px-12 py-4">
       <div>
-        <Image src={logo} height={70} alt='Migo logo'/>
+        <Image src={logo} height={70} alt="Migo logo" />
       </div>
 
-      <div className='flex gap-5 text-white font-semibold text-lg'>
-        <Link href="#">Inicío</Link>
-        <Link href="#">Planos</Link>
-        <Link href="#">Contato</Link>
-        <Link href="#">Ajuda</Link>
+      <div className="block sm:hidden">
+        <Sheet>
+          <SheetTrigger>
+            <AlignJustify color="white" size={30} />
+          </SheetTrigger>
+          <SheetContent className="w-[400px] sm:w-[540px]">
+            <SheetHeader>
+              <SheetDescription>
+                <div className="flex flex-col items-center gap-3 text-xl font-semibold text-black">
+                  <Links />
+                </div>
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </div>
+
+      <div className="hidden gap-5 text-white font-semibold text-lg sm:flex">
+        <Links />
       </div>
     </header>
   );
---
+++ src/components/Links.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Links() {
+  return (
+    <>
+      <Link href="#">Inicío</Link>
+      <Link href="#">Planos</Link>
+      <Link href="#">Contato</Link>
+      <Link href="#">Ajuda</Link>
+    </>
+  );
+}
