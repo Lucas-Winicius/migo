2b251dc7c3619819f9855e5d867ccb552381ed79
Lucas-Winicius <lucasclucas7@gmail.com>
2024-05-19 22:39:56 -0300

    main title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/Header";
+import MainTitle from "@/components/MainTitle";
 
 export default function Home() {
   return (
-    <main>
+    <div>
       <Header />
-    </main>
+      <MainTitle />
+    </div>
   );
 }
---
+++ src/components/MainTitle.tsx
@@ -0,0 +1,7 @@
+export default function MainTitle() {
+  return (
+    <div className="bg-migo h-44 flex items-center justify-center text-emerald-950 font-bold md:text-5xl text-3xl px-5 text-center">
+      <h1>Uma nova forma de se conectar ao mundo!</h1>
+    </div>
+  );
+}
